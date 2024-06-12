ffabbf277b95324530568658c46d67959d1b7746
soso0024 <131151398+soso0024@users.noreply.github.com>
2024-06-12 17:26:36 +0200

    add: finish

--- biginners_selection/ABC085C.py
+++ biginners_selection/ABC085C.py
@@ -4,26 +4,45 @@ num_ichiman = 0
 num_gosen = 0
 num_senen = 0
 
-for i in range(1, 2000):
-    if y >= 10000:
-        y -= 10000
-        print(y)
-        num_ichiman += 1
-    else:
-        break
+for i in range(n + 1):
+    for j in range(n + 1 - i):
+        k = n - i - j
+        if (10000 * i + 5000 * j + 1000 * k) == y:
+            num_ichiman = i
+            num_gosen = j
+            num_senen = k
+            # print(i, j, k)
+            break
+    if (num_ichiman + num_gosen + num_senen) == n:
+        print(num_ichiman, num_gosen, num_senen)
+        exit()
 
-for i in range(1, 2000):
-    if y > 5000:
-        y -= 5000
-        num_gosen += 1
-    else:
-        break
+print(-1, -1, -1)
 
-for i in range(1, 2000):
-    if y >= 1000:
-        y -= 1000
-        num_senen += 1
-    else:
-        break
 
-print(num_ichiman, num_gosen, num_senen)
+# for i in range(1, 2000):
+#     if y >= 10000:
+#         y -= 10000
+#         print(y)
+#         num_ichiman += 1
+#         tol_n += 1
+#     else:
+#         break
+
+# for i in range(1, 2000):
+#     if y > 5000:
+#         y -= 5000
+#         num_gosen += 1
+#         tol_n += 1
+#     else:
+#         break
+
+# for i in range(1, 2000):
+#     if y >= 1000:
+#         y -= 1000
+#         num_senen += 1
+#         tol_n += 1
+#     else:
+#         break
+
+# print(num_ichiman, num_gosen, num_senen, tol_n)
