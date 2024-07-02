b21c976dc81f65d4618c8980b916c8a19e281386
soso0024 <131151398+soso0024@users.noreply.github.com>
2024-07-02 22:28:44 +0200

    add:

--- abc360/a.py
+++ abc360/a.py
@@ -1,1 +1,19 @@
+S = input()
 
+if len(S) != 3:
+    print("error")
+    exit()
+
+rice_idx = 0
+miso_idx = 0
+
+for i in range(len(S)):
+    if S[i] == "R":
+        rice_idx = i
+    elif S[i] == "M":
+        miso_idx = i
+
+if rice_idx < miso_idx:
+    print("Yes")
+else:
+    print("No")
